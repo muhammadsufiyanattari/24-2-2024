4699b75e3bf6575e3e88becf88cdf55236b1c7e8
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-02-27 11:02:43 +0500

    added all

---
+++ app.js
@@ -0,0 +1,79 @@
+// chapter-1
+// Q1
+// alert("Error!Please enter a currect password")
+
+// chapter-1
+// Q2
+// alert("wellcome to js land \n happy coding!")
+
+// chapter-1
+// Q3
+// alert("wellcome to js land....")
+
+
+// chapter-1
+// Q4
+// console.log("hello world")
+
+// complete-chapter-01
+
+
+//chapter-02
+//Q1
+// var username=("SUFIYAN");
+
+// alert(username);
+//Q2
+// var fullName=("Jhon dukkte");
+
+// alert(fullName);
+//Q3
+
+// var students=("web and app mobile app")
+// alert(students)
+//Q4
+
+// var sQue=("plzzz\n plz \n pl \n z \n z \n z")
+
+// alert(sQue);
+//Q5
+// var email=("example email@gmail.com")
+// alert(email)
+
+// //Q6
+// var book=("A smart way to learn javascript")
+// alert("Iam typing to learn form " + book)
+
+//Q7
+// document.write("Hello Hay! How arr You")
+
+//Q8
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// complete chapter 3
+
+
+// chapter-3
+// Q1
+// var age=("I am 20 years old");
+// alert(age);
+
+// Q2
+// var num=14;
+
+// alert("You have visit this site "+num+" time");
+
+
+// Q3
+// var brithYear= ("My birth year is 8 march 2004")
+
+// document.write(brithYear +"<br> date type of declear variable is  number")
+// Q4
+// alert("www.xyz clothing.com")
+// var a="Visit's Name"
+// var b=("product title")
+// var c=("Quantity i.e how many visitor product when to order")
+
+
+
+// document.write("<b>Jhon Doe</b> order <b>5 shirts</b>s on xyz clothing store")
